c5d8375be7379a8d45ec6d3d4a130b6b38d47f03
Jay Sikkeland <36973689+jaysikkeland@users.noreply.github.com>
2018-08-13 23:29:36 -0700

    Update step1_papisample_listgroups.py

--- step1_papisample_listgroups.py
+++ step1_papisample_listgroups.py
@@ -27,7 +27,7 @@ from akamai.edgegrid import EdgeGridAuth, EdgeRc
 # --- REPLACE BEGIN ---
 #
 # Edgegrid credentials file and section
-edgerc = EdgeRc('/Users/jsikkela/.edgerc')
+edgerc = EdgeRc('.edgerc')
 # Edgegrid credentials section
 section = 'default'
 # Group you want to print separately
